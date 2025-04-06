d1affbc650003eb1b51fdc5a29b8aeb1e33d2303
aj-arts <ajinkyagokule@gmail.com>
2025-04-06 02:55:47 -0700

    added route.py route optimizer

---
+++ server/matching/route.py
@@ -0,0 +1,86 @@
+import requests
+from datetime import datetime, timedelta
+
+class RouteOptimization:
+    def __init__(self, project_id, oauth_token):
+        self.project_id = project_id
+        self.oauth_token = oauth_token
+
+    def add_one_day(self, timestamp_str):
+        timestamp = datetime.strptime(timestamp_str, "%Y-%m-%dT%H:%M:%SZ")
+        new_timestamp = timestamp + timedelta(days=2)
+        return new_timestamp.strftime("%Y-%m-%dT%H:%M:%SZ")
+    
+    def optimize_tours(self, vehicle_waypoint, shipment_waypoints, start_time):
+        vehicle = {
+            "startWaypoint": {"placeId": vehicle_waypoint[0]},
+            "endWaypoint": {"placeId": vehicle_waypoint[1]},
+            "travelMode": "DRIVING"
+        }
+        
+        shipments = []
+        for pickup, delivery in shipment_waypoints:
+            shipments.append({
+                "pickups": [
+                    {"arrivalWaypoint": {"placeId": pickup}}
+                ],
+                "deliveries": [
+                    {"arrivalWaypoint": {"placeId": delivery}}
+                ]
+            })
+        
+        end_time = self.add_one_day(start_time)
+        
+        shipment_model = {
+            "shipments": shipments,
+            "vehicles": [vehicle],
+            "globalStartTime": start_time,
+            "globalEndTime": end_time
+        }
+        
+        optimization_request = {
+            "model": shipment_model,
+            "considerRoadTraffic": True,
+            "populatePolylines": True
+        }
+        
+        url = f"https://routeoptimization.googleapis.com/v1/projects/{self.project_id}:optimizeTours"
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {self.oauth_token}"
+        }
+        
+        response = requests.post(url, json=optimization_request, headers=headers)
+        
+        if response.status_code != 200:
+            raise Exception(f"Error: {response.status_code}, {response.text}")
+        
+        response = response.json()
+        total_duration = response["metrics"]["aggregatedRouteMetrics"]["totalDuration"]
+        total_distance = response["metrics"]["aggregatedRouteMetrics"]["travelDistanceMeters"]
+        polyline = response["routes"][0]["routePolyline"]["points"]
+
+        total_duration = int(total_duration[:-1])
+        total_distance = int(total_distance)
+
+        output = {
+            "total_duration": total_duration,
+            "total_distance": total_distance,
+            "polyline": polyline
+        }
+
+        return output
+    
+if __name__ == "__main__":    
+    vehicle_waypoint = ("ChIJfdcUqp1AwFQRvsC9Io-ADdc", "ChIJJ3SpfQsLlVQRkYXR9ua5Nhw")
+    shipment_waypoints = [
+        ("ChIJv0fYEV4XwFQRAKdgafDZ1R8", "ChIJY5xLvPz-v1QRwlcDj-ApNPk"),
+        ("ChIJ3VwciHg3wFQR1iwHMt0kbkY", "ChIJeeW8Vl8FlVQRhu0zaob_KX0")
+    ]
+    start_time = "2025-04-06T08:00:00Z"
+    project_id = "valued-decker-387606"
+    oauth2_token = "ya29.a0AZYkNZj_dJ082wovtf_qD7HtF6rJJBRrczL9ZBDG2yBJFW4_AQtu6KWJaHwoUKBVLtcpDwXs42Lr7KhYGVU97cYzpcnWyEZ-_Itw15as4P5wJv_IJSOpLeBQvvNjclznIdlyNLSFGGkSSzw-KFmdyc3TsD8btC4eDdRiO78cRD8CqAaCgYKAZsSARISFQHGX2MiigdJlJdecWjt6kda71jiBQ0181"
+
+    route_optimizer = RouteOptimization(project_id, oauth2_token)
+    result = route_optimizer.optimize_tours(vehicle_waypoint, shipment_waypoints, start_time)
+    print(result)
