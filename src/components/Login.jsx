a7db68f482775d4053b56bdc419588a16359fa54
David Gesl <davidgesl123@gmail.com>
2025-04-06 04:57:08 -0700

    changed naming convention to match what AJ put on server

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -33,7 +33,7 @@ const Login = () => {
 const App = () => {
     console.log("BALALSDMALKSDMASLKDJASLDKJASLKDJASLKDJASLKDJALSDJASLKDJLKASJDALKSJDLAKSJDALSKDJSAKJDKSAD"); 
   return (
-    <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
+    <GoogleOAuthProvider clientId={process.env.CLIENT_ID}>
       <Login />
     </GoogleOAuthProvider>
   );
